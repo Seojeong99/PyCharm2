4f466a1e1729e48e97fbd6292454efca6b75b13e
Seojeong99 <pinkberry99@naver.com>
2021-03-12 16:51:18 +0900

    21.03.12

--- similarity.py
+++ similarity.py
@@ -4,10 +4,40 @@ import time
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 
+from main import Jaccard_similarity
+
 driver = webdriver.Firefox(executable_path="C:/driver/geckodriver.exe")
 driver.wait = WebDriverWait(driver, 2)
-
+count1 = 0
+count2 = 0
 def checksimilarity(keyword):
+    global count1
+    global count2
+    URL3 = "https://translate.google.com/?sl=ko&tl=en&op=translate"
+    driver.get(URL3)
+    time.sleep(3)
+
+    driver.find_element_by_css_selector('.er8xn').clear()
+    time.sleep(3)
+    str1 = str("이것은 " + keyword + "이다.")
+    driver.find_element_by_css_selector('.er8xn').send_keys(str1)
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    result1 = soup.find('div', class_='NqnNQd').text
+
+    URL4 = "https://translate.google.com/?sl=en&tl=ko&op=translate"
+    driver.get(URL4)
+    time.sleep(3)
+    driver.find_element_by_css_selector('.er8xn').clear()
+    time.sleep(3)
+    driver.find_element_by_css_selector('.er8xn').send_keys(result1)
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    result2 = soup.find('div', class_='NqnNQd').text
+    print('<original>' + str1 + '<original tr>' + str(result1) + '<final tr>' + str(result2))
+    #print(Jaccard_similarity(str1, str(result2)))
+    count1 = count1 + Jaccard_similarity(str1, str(result2))
+
 
 
 with open("input.txt", "r", encoding="UTF-8") as f:
@@ -16,3 +46,15 @@ with open("input.txt", "r", encoding="UTF-8") as f:
             try:
                 checksimilarity(data)
             except: 0
+
+print(round(count1 / 400, 4))
+count1 = 0
+
+with open("inputfinal.txt", "r", encoding="UTF-8") as f:
+    list = f.readlines()
+    for i, data in enumerate(list):
+            try:
+                checksimilarity(data)
+            except: 0
+
+print(round(count1 / 400, 4))
