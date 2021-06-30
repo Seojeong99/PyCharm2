d2367fd15c6869e068a00ad1140927ad9daba978
Seojeong99 <pinkberry99@naver.com>
2021-06-30 23:33:19 +0900

    21.06.30

--- ant.py
+++ ant.py
@@ -1,3 +1,4 @@
+
 pan=[]
 ipan=[]
 pan=[list(map(int, input().split())) for _ in range(10)]
@@ -24,10 +25,12 @@ for i in range(1,9):
     for j in range(1,9):
         if ipan[i][j]==0:
             pan[i][j]=9
+            print(i, j)
             print("ㅇㅋ")
         else:
             i=i+1
-            print("ㄴ")
+            if ipan[i][j-1]==0:
+                pan[i][j-1]=9
 
 i=0
 j=0
@@ -36,3 +39,12 @@ for i in range(10):
         print(pan[i][j],end=' ')
     print()
 #출력하기
+
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        if j == 3:
+            i = i + 1
+            cycle(i,j)
+
+
